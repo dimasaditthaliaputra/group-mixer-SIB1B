a7d8c7143134402c2c68fbaf35dde007e0887422
dimasaditt <98073807+dimasaditthaliaputra@users.noreply.github.com>
2025-06-11 13:56:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       const kelompok = [];
 
       const abel = data.find((m) => m.id === 2);
-      const kandidatTeman = [6]
+      const kandidatTeman = [17]
         .map((id) => data.find((m) => m.id === id))
         .filter(Boolean);
 
